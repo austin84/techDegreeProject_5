d0cd7eb23cee6993cf3adc09101efd9f882999af
austin84 <austinveatch84@gmail.com>
2020-04-22 11:00:19 -0700

    lightbox and search functioning

--- js/scripts.js
+++ js/scripts.js
@@ -10,7 +10,34 @@ lightbox.option({
 
 // Search Feature
 
-// $("input").on("keyup", function () {
-//   let search = $(this).value().toUpperCase();
-//   console.log(search);
+$(document).ready(function () {
+  $("#search").hideseek({
+    list: ".list",
+    nodata: "",
+    attribute: "title",
+    matches: true,
+    highlight: false,
+    ignore: "",
+    navigation: false,
+    ignore_accents: false,
+    hidden_mode: false,
+    min_chars: 1,
+  });
+});
+
+// const input = $("#search").val().toUpperCase();
+// const gallery = $(".photo");
+// const data = $(".photo a").attr("data-title").toUpperCase();
+
+// $("#search").keyup(function () {
+//   let search = $("#search").val().toUpperCase();
+//   let gallery = $(".photo");
+//   for (let i = 0; i < gallery.length; i += 1) {
+//     let data = $(".photo a"[i]).attr("data-title").toUpperCase();
+//     if (search == data) {
+//       gallery[i].style.display = "none";
+//     } else {
+//       gallery[i].style.display = "block";
+//     }
+//   }
 // });
