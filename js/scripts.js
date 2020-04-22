8ef11470e4bb9f864c196f94c87aaa7a7f5111dd
austin84 <austinveatch84@gmail.com>
2020-04-21 22:10:13 -0700

    break

--- js/scripts.js
+++ js/scripts.js
@@ -2,9 +2,10 @@
 
 lightbox.option({
   resizeDuration: 500,
-  wrapAround: true,
   alwaysShowNavOnTouchDevices: true,
-  disableScrolling: true,
   showImageNumberLabel: true,
   maxWidth: 1200,
+  wrapAround: true,
 });
+
+// Search Feature
