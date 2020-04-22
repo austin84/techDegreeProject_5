bfa781a4cd6f9e5aff11c55b2342527af11d1f03
austin84 <austinveatch84@gmail.com>
2020-04-21 21:53:37 -0700

    install lightbox and adjust js files

---
+++ js/scripts.js
@@ -0,0 +1,10 @@
+// Lightbox Options
+
+lightbox.option({
+  resizeDuration: 500,
+  wrapAround: true,
+  alwaysShowNavOnTouchDevices: true,
+  disableScrolling: true,
+  showImageNumberLabel: true,
+  maxWidth: 1200,
+});
